bd5dc9199240e403cd4ee2612f87c35a025775ff
Raquel Randall <raquel.sae.randall@gmail.com>
2018-10-09 21:18:19 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,12 +2,31 @@ def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |categories, values|
     values.each do |traits, names|
-      new_hash[names] = {
-        :color => [],
-        :gender => [],
-        :lives => []
-      }
+      names.each do |name|
+        new_hash[name] = {
+          :color => [],
+          :gender => [],
+          :lives => []
+        }
+      end
     end
   end
+  keys = new_hash.keys
+  data[:color].each do |color, name|
+    name.each do |bird_name|
+      key.each do |key|
+        if bird_name == key
+          new_hash[key][:color] << color.to_s
+        end
+      end
+    end
+  end
+  
+
+
+
+
+
+  
   new_hash
 end
